1d6855ecfcb83b46ead4f504527a94b662255aef
alem25 <alem2518@hotmail.com>
2021-04-23 23:26:32 +0200

    v23 getTotalFrequentRenterPoints now uses lambda function instead of loop while

--- src/main/java/usantatecla/movies/v21/Customer.java
+++ src/main/java/usantatecla/movies/v21/Customer.java
@@ -41,13 +41,6 @@ public class Customer {
 	}
 	
 	private int getTotalFrequentRenterPoints() {
-		int result = 0;
-		Iterator<Rental> rentals = this.rentals.iterator();
-		while (rentals.hasNext()) {
-			Rental each = rentals.next();
-			result += each.getFrequentRenterPoints();
-		}
-		return result;
+		return this.rentals.stream().map(Rental::getFrequentRenterPoints).reduce(0, (result, frequentRenterPoints) -> result + frequentRenterPoints);
 	}
-
 }
